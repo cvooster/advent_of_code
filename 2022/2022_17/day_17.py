a033b0c888a903f94a90c38e692a3b8d447abdc5
cvooster <130979903+cvooster@users.noreply.github.com>
2023-06-27 12:16:39 +0200

    Cosmetic changes in solution day 17.

--- 2022/2022_17/day_17.py
+++ 2022/2022_17/day_17.py
@@ -31,34 +31,33 @@ def main():
 
 def simulate_falling_rocks(filename, nr_rocks):
     """Simulate the falling of rocks, and obtain the tower height."""
-    chamber, shapes, jet_pattern = initialize_simulation(filename)
-    jet_idx = 0
-    shape_idx = 0
-    depths = [[VERT_DIST] * CHAMBER_WIDTH]
-    height = 0
-    state_hist = [(jet_idx, shape_idx, depths)]
-    height_hist = [height]
+    chamber, jet_pattern, shapes = initialize_simulation(filename)
     has_repeated = False
 
-    for i in range(nr_rocks):
+    jet_idx, shape_idx, depths = 0, 0, tuple([VERT_DIST] * CHAMBER_WIDTH)
+    state_hist = {(jet_idx, shape_idx, depths): 0}
+    height = 0
+    height_hist = [height]
+
+    for i in range(1, nr_rocks + 1):
         shape = shapes[shape_idx]
-        jet_idx = simulate_single_rock(chamber, shape, jet_pattern, jet_idx)
+        jet_idx = simulate_single_rock(chamber, jet_pattern, jet_idx, shape)
         shape_idx = (shape_idx + 1) % len(SHAPE_UNITS)
         depths = determine_depths(chamber)
         height = len(chamber) - VERT_DIST
 
         if not has_repeated:
             previous = check_repetition(state_hist, jet_idx, shape_idx, depths)
-            if previous is not None:
+            if previous is None:
+                state_hist[(jet_idx, shape_idx, depths)] = i
+            elif previous is not None:
                 has_repeated = True
-                cycle_length = (i + 1) - previous
+                cycle_length = i - previous
                 cycle_height_increase = height - height_hist[previous]
                 transient_length = nr_rocks % cycle_length
 
-        state_hist.append((jet_idx, shape_idx, depths))
         height_hist.append(height)
-
-        if has_repeated and i >= transient_length - 1:
+        if has_repeated and i >= transient_length:
             height_transient = height_hist[transient_length]
             height_cycles = (nr_rocks // cycle_length) * cycle_height_increase
             height = height_transient + height_cycles
@@ -67,45 +66,36 @@ def simulate_falling_rocks(filename, nr_rocks):
 
 
 def initialize_simulation(filename):
-    """Initialize the chamber, the shapes, and the jet pattern."""
+    """Initialize the chamber, the jet pattern, and the shapes."""
     chamber = [[False] * CHAMBER_WIDTH for _ in range(VERT_DIST)]
-    shapes = []
-    for units in SHAPE_UNITS:
-        shapes.append(Shape(units))
     jet_pattern = aoc.read_stripped(filename)
-    return chamber, shapes, jet_pattern
+    shapes = [Shape(units) for units in SHAPE_UNITS]
+    return chamber, jet_pattern, shapes
 
 
 def determine_depths(chamber):
     """Determine the depth of the latest stopped rock in every column."""
     depths = []
     for col in range(CHAMBER_WIDTH):
-        depth = VERT_DIST
-        while len(chamber) - depth >= 0:
-            if not chamber[len(chamber) - depth][col]:
-                depth += 1
-            else:
-                break
-        depths.append(depth)
-    return depths
+        row = len(chamber) - VERT_DIST
+        while not chamber[row][col] and row > 0:
+            row -= 1
+        depths.append(len(chamber) - row)
+    return tuple(depths)
 
 
 def check_repetition(state_history, jet_idx, shape_idx, depths):
     """Check whether jet index, shape, and depths have occurred before."""
-    try:
-        previous = state_history.index((jet_idx, shape_idx, depths))
-    except ValueError:
-        previous = None
-    return previous
+    return state_history.get((jet_idx, shape_idx, depths))
 
 
-def simulate_single_rock(chamber, shape, jet_pattern, jet_idx):
+def simulate_single_rock(chamber, jet_pattern, jet_idx, shape):
     """Simulate the falling of a rock."""
     bottom_left_x = HORZ_DIST
     bottom_left_y = len(chamber) - 1
 
     # Initially, no need to check floor or stopped rock collisions:
-    for _ in range(VERT_DIST - 1):
+    for _ in range(VERT_DIST):
         if jet_pattern[jet_idx] == "<":
             if bottom_left_x > 0:
                 bottom_left_x -= 1
@@ -113,34 +103,35 @@ def simulate_single_rock(chamber, shape, jet_pattern, jet_idx):
             if bottom_left_x < CHAMBER_WIDTH - shape.width:
                 bottom_left_x += 1
         jet_idx = (jet_idx + 1) % len(jet_pattern)
+        if bottom_left_y == len(chamber) - VERT_DIST:
+            break
         bottom_left_y -= 1
 
     # Thereafter, check for floor or stopped rock collisions:
-    while True:
+    while bottom_left_y > 0 and not any(
+        chamber[bottom_left_y + u[0] - 1][bottom_left_x + u[1]]
+        for u in shape.units
+    ):
+        bottom_left_y -= 1
         if jet_pattern[jet_idx] == "<":
             if bottom_left_x > 0 and not any(
-                chamber[bottom_left_y + unit[0]][bottom_left_x + unit[1] - 1]
-                for unit in shape.units
+                chamber[bottom_left_y + u[0]][bottom_left_x + u[1] - 1]
+                for u in shape.units
             ):
                 bottom_left_x -= 1
         elif jet_pattern[jet_idx] == ">":
             if bottom_left_x < CHAMBER_WIDTH - shape.width and not any(
-                chamber[bottom_left_y + unit[0]][bottom_left_x + unit[1] + 1]
-                for unit in shape.units
+                chamber[bottom_left_y + u[0]][bottom_left_x + u[1] + 1]
+                for u in shape.units
             ):
                 bottom_left_x += 1
         jet_idx = (jet_idx + 1) % len(jet_pattern)
-        if bottom_left_y > 0 and not any(
-            chamber[bottom_left_y + unit[0] - 1][bottom_left_x + unit[1]]
-            for unit in shape.units
-        ):
-            bottom_left_y -= 1
-        else:
-            break
-    for unit in shape.units:
-        chamber[bottom_left_y + unit[0]][bottom_left_x + unit[1]] = True
 
-    # In the chamber, ensure VERT_DIST rows on top of highest stopped rock:
+    # The rock has come to rest:
+    for u in shape.units:
+        chamber[bottom_left_y + u[0]][bottom_left_x + u[1]] = True
+
+    # Add rows to chamber to ensure VERT_DIST on top of highest stopped rock:
     if bottom_left_y + shape.height > len(chamber) - VERT_DIST:
         for _ in range(bottom_left_y + shape.height - len(chamber) + VERT_DIST):
             chamber.append([False] * CHAMBER_WIDTH)
@@ -153,8 +144,11 @@ class Shape:
     def __init__(self, units):
         """Create a shape, and set its height and width."""
         self.units = units
-        self.height = max(unit[0] for unit in self.units) + 1
-        self.width = max(unit[1] for unit in self.units) + 1
+        self.height = max(u[0] for u in self.units) + 1
+        self.width = max(u[1] for u in self.units) + 1
+
+        # Violating the following condition would lead to IndexErrors later on:
+        assert self.height <= VERT_DIST
 
 
 if __name__ == "__main__":
